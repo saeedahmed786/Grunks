980ee240b964c009df6b8859e2d84f47ed392451
Etharibrahim <ibrahimethar0@gmail.com>
2022-06-06 00:25:33 +0500

    last

--- src/components/CenterBox.js
+++ src/components/CenterBox.js
@@ -12,7 +12,7 @@ export const CenterBox = () => {
                 </div>
                 <div className='columns'>
                     <div>
-                        <div className='amount d-flex align-items-center justify-content-between gap-5 py-2'>
+                        <div className='amount d-flex align-items-center justify-content-between gap-0 gap-sm-5 py-2'>
                             <div>Amount</div>
                             <div className='add d-flex align-items-center gap-2'>
                                 <div><a onClick={() => setMint(mint >= 2 ? mint - 1 : mint)}>-</a></div>
@@ -20,13 +20,13 @@ export const CenterBox = () => {
                                 <div><a onClick={() => setMint(mint + 1)}>+</a></div>
                             </div>
                         </div>
-                        <div className='vertical d-none d-sm-block'></div>
+                        <div className='vertical'></div>
                         <div className='right-part p-2'>
                             <div className='d-flex align-items-center gap-2'>
-                                <div className='d-none d-sm-block'>or</div>
+                                <div className=''>or</div>
                                 <div className='btn-container'>
-                                    <button className='btn d-none d-sm-block'>5</button>
-                                    <button className='btn d-none d-sm-block'>10</button>
+                                    <button className='btn '>5</button>
+                                    <button className='btn '>10</button>
                                     <button className='btn'>Max</button>
                                 </div>
                             </div>
